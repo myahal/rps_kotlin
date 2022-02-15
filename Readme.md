d27817650e45ec1f111c46fcb36f674da406b13b
Chiharu Miya <miya.chiharu@gmail.com>
2022-02-15 10:50:12 +0900

    First commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# XP WS Template
+
+XP Workshop１日目のためのKotlin版テンプレートです。
+
+rock, paper, scissorsをTDDを通して実装していきます。
---
+++ src/test/kotlin/WiringTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class WiringTest {
+    @Test
+    fun wiringTest() {
+        assertTrue(true)
+    }
+}
