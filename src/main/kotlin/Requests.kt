e2015bab681b5912df6f39d8629c7ed014fadaff
Chiharu Miya <miya.chiharu@gmail.com>
2022-02-17 18:44:15 +0900

    勝敗判定のメソッドを切り出し

--- src/main/kotlin/Requests.kt
+++ src/main/kotlin/Requests.kt
@@ -10,16 +10,24 @@ interface Observer {
 
 class Requests {
     fun play(p1: Throws, p2: Throws, observer: Observer): Unit {
-        if (p1 == p2) {
+        if (tie(p1, p2)) {
             observer.tie()
             return
         }
-        if (p1 == Throws.ROCK && p2 == Throws.SCISSORS
-            || p1 == Throws.SCISSORS &&  p2 == Throws.PAPER
-            || p1 == Throws.PAPER && p2 == Throws.ROCK) {
+        if (p1Wins(p1, p2)) {
             observer.p1Wins()
         } else {
             observer.p2Wins()
         }
+    }
+
+    private fun tie(p1: Throws, p2: Throws): Boolean {
+        return p1 == p2
+    }
+
+    private fun p1Wins(p1: Throws, p2: Throws): Boolean {
+        return p1 == Throws.ROCK && p2 == Throws.SCISSORS
+                || p1 == Throws.SCISSORS &&  p2 == Throws.PAPER
+                || p1 == Throws.PAPER && p2 == Throws.ROCK
     }
 }
